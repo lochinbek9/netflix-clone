23a6c170c7e2623dd18ea07470f34c61c3ca2991
lochinbek9 <ldehkonov9@gmail.com>
2025-06-03 23:29:02 +0500

    Starter papkalar tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
-
-  console.log("Hello");
   
   return (
     <>
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,10 @@
+
+import "./Player.css"
+
+function Player() {
+  return (
+    <div className="player">Player</div>
+  )
+}
+
+export default Player
